e287152f8415b29c6bb6489aaecd255df5dca74a
jacksonllee <jacksonlunlee@gmail.com>
2017-03-12 18:19:04 -0500

    Download gedit darcula theme

--- ubuntu/install_packages.sh
+++ ubuntu/install_packages.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
-# This script is to be run *not* directly, but invoked by ../install.sh (be careful with path issues).
 
-cat $PWD/ubuntu/packages.txt | xargs sudo apt-get install
+# Get Darcula theme for gedit
+$GEDITSTYLES='~/.local/share/gedit/styles/'
+mkdir -p $GEDITSTYLES
+wget -O $GEDITSTYLES/theme_darcula.xml https://raw.githubusercontent.com/Sash0k/darcula-theme-various-ide/master/GtkSourceView/2.0/theme_darcula.xml
+
+# Install packages through apt-get
+cat packages.txt | xargs sudo apt-get install
