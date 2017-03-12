c551888587c6e2950d7a1b0961aeeba60556a971
jacksonllee <jacksonlunlee@gmail.com>
2017-03-12 18:20:48 -0500

    Update brew install script

--- brew/install_packages.sh
+++ brew/install_packages.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
-# This script is to be run *not* directly, but invoked by ../install.sh (be careful with path issues)
 
-cat $PWD/brew/packages.txt | xargs brew install
+cat packages.txt | xargs brew install
