f7cc696e9e9a337a6aef3ee63812a14d48668252
jacksonllee <jacksonlunlee@gmail.com>
2017-03-12 19:16:32 -0500

    Turn on darcula color scheme

--- vim/configure.sh
+++ vim/configure.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 
-# download the darcula color scheme
+# Download the darcula color scheme
 mkdir -p ~/.vim/colors
 wget -O ~/.vim/colors/darcula.vim https://raw.githubusercontent.com/blueshirts/darcula/master/colors/darcula.vim
+
+# Create ~/.vimrc and turn on the darcula color scheme
+touch ~/.vimrc
+echo 'syntax on'$'\n''colorscheme darcula' > ~/.vimrc
 
