29e24fff5905373feeb3940976f8e81eed30d68f
jacksonllee <jacksonlunlee@gmail.com>
2017-11-19 13:38:53 -0600

    Fix commands for getting gedit theme on ubuntu

--- ubuntu/install_packages.sh
+++ ubuntu/install_packages.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
 
 # Get Darcula theme for gedit
-$GEDITSTYLES='~/.local/share/gedit/styles/'
-mkdir -p $GEDITSTYLES
-wget -O $GEDITSTYLES/theme_darcula.xml https://raw.githubusercontent.com/Sash0k/darcula-theme-various-ide/master/GtkSourceView/2.0/theme_darcula.xml
+mkdir -p ~/.local/share/gedit/style
+wget -O ~/.local/share/gedit/style/theme_darcula.xml https://raw.githubusercontent.com/Sash0k/darcula-theme-various-ide/master/GtkSourceView/2.0/theme_darcula.xml
 
 # Install packages through apt-get
 cat packages.txt | xargs sudo apt-get install
